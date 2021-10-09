7b690397c945ca2001c692e0227b72ade5fee1bb
Korsaurus <grkorsos@gmail.com>
2021-10-08 19:39:21 -0600

    Added a fun but basic theme switcher

--- Script.js
+++ Script.js
@@ -1,14 +1,14 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
 const WINNING_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
@@ -22,68 +22,117 @@ startGame()
 restartButton.addEventListener('click', startGame)
 
 function startGame() {
-  circleTurn = false
-  cellElements.forEach(cell => {
-    cell.classList.remove(X_CLASS)
-    cell.classList.remove(CIRCLE_CLASS)
-    cell.removeEventListener('click', handleClick)
-    cell.addEventListener('click', handleClick, { once: true })
-  })
-  setBoardHoverClass()
-  winningMessageElement.classList.remove('show')
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click', handleClick)
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
 }
 
 function handleClick(e) {
-  const cell = e.target
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-  placeMark(cell, currentClass)
-  if (checkWin(currentClass)) {
-    endGame(false)
-  } else if (isDraw()) {
-    endGame(true)
-  } else {
-    swapTurns()
-    setBoardHoverClass()
-  }
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+        endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurns()
+        setBoardHoverClass()
+    }
 }
 
 function endGame(draw) {
-  if (draw) {
-    winningMessageTextElement.innerText = 'Draw!'
-  } else {
-    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
-  }
-  winningMessageElement.classList.add('show')
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!'
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
 }
 
 function isDraw() {
-  return [...cellElements].every(cell => {
-    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-  })
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    })
 }
 
 function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass)
+    cell.classList.add(currentClass)
 }
 
 function swapTurns() {
-  circleTurn = !circleTurn
+    circleTurn = !circleTurn
 }
 
 function setBoardHoverClass() {
-  board.classList.remove(X_CLASS)
-  board.classList.remove(CIRCLE_CLASS)
-  if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS)
-  } else {
-    board.classList.add(X_CLASS)
-  }
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
 }
 
 function checkWin(currentClass) {
-  return WINNING_COMBINATIONS.some(combination => {
-    return combination.every(index => {
-      return cellElements[index].classList.contains(currentClass)
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
     })
-  })
+}
+
+// document.getElementById("MyElement").classList.add('MyClass');
+
+// document.getElementById("MyElement").classList.remove('MyClass');
+
+// if ( document.getElementById("MyElement").classList.contains('MyClass') )
+
+// document.getElementById("MyElement").classList.toggle('MyClass');
+const grassThemeSwitcher = document.getElementById("grass-theme")
+grassThemeSwitcher.onclick = function () {
+    document.getElementById("body").classList.remove('wars');
+    document.getElementById("body").classList.remove('stars');
+    document.getElementById("body").classList.remove('clouds');
+    document.getElementById("body").classList.remove('starwars');
+    document.getElementById("body").classList.add('grass');
+}
+
+const starsThemeSwitcher = document.getElementById("stars-theme")
+starsThemeSwitcher.onclick = function () {
+    document.getElementById("body").classList.remove('grass');
+    document.getElementById("body").classList.remove('clouds');
+    document.getElementById("body").classList.add('stars');
+    if ( document.getElementById("body").classList.contains('wars') ) {
+        document.getElementById("body").classList.remove('wars');
+        document.getElementById("body").classList.remove('stars');
+        document.getElementById("body").classList.add('starwars');
+    }
+}
+
+const cloudsThemeSwitcher = document.getElementById("clouds-theme")
+cloudsThemeSwitcher.onclick = function () {
+    document.getElementById("body").classList.remove('wars');
+    document.getElementById("body").classList.remove('stars');
+    document.getElementById("body").classList.remove('grass');
+    document.getElementById("body").classList.remove('starwars');
+    document.getElementById("body").classList.add('clouds');
+}
+
+const warsThemeSwitcher = document.getElementById("wars-theme")
+warsThemeSwitcher.onclick = function () {
+    document.getElementById("body").classList.remove('clouds');
+    document.getElementById("body").classList.remove('grass');
+    document.getElementById("body").classList.add('wars');
+    if ( document.getElementById("body").classList.contains('stars') ) {
+        document.getElementById("body").classList.remove('stars');
+        document.getElementById("body").classList.remove('wars');
+        document.getElementById("body").classList.add('starwars');
+    }
 }
